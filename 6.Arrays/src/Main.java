59e2bdc2a395e43f4ecef5f98030490b8d9ac247
Sahil Deshmukh <sahildeshmukhofficial45@gmail.com>
2026-08-04 23:34:45 +0530

    Arrays

---
+++ 6.Arrays/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // contigeous memory means continues memory allocation in heap means one after one memory has allocated
+        int[] op = new int[5];
+
+        int[] nums = {0,1,2,3,4,5,6};
+
+        int[][] marks = new int[3][3];
+
+        //in 2d array every element is array itself
+        for(int row=0;row<marks.length;row++){
+            for(int col=0;col<marks[row].length;col++){
+                System.out.println( marks[row][col]);
+            }
+        }
+
+//        Primitives (int, float, double, etc.): Stored directly in the Stack memory.
+//        If you write int x = 4;, a 4-byte block of memory on the stack is allocated, and the binary value of 4 is placed directly inside it.
+//        Non-Primitives (Arrays, Objects, Strings): Created using the new keyword. These are stored in the Heap memory. When you write int[] arr = new int[5];:
+//        A continuous block of memory for 5 integers (5 x 4 bytes = 20 bytes) is reserved in the Heap.
+//        A reference variable named arr is created in the Stack.
+//        This Stack variable does not contain the array. It contains a memory address pointer to the start of the array in the Heap (the base address).
+//        Arrays provide "Random Access", meaning accessing arr[2] takes the exact same amount of time as accessing arr[200].
+//        The JVM doesn't count from zero to reach the index; it calculates the physical memory address instantly using a formula:
+//        Target Address = Base_Address + (Data_Type_Size * Index)
+//        If an integer array starts at memory address 100 (the Base Address):
+//        arr[0]: 100 + (4 bytes * 0) = Address 100
+//        arr[3]: 100 + (4 bytes * 3) = Address 112
+//        The JVM jumps straight to address 112 and reads 4 bytes. This contiguous memory layout is also highly optimized for CPU Caching (fetching adjacent memory blocks in advance).
+//        Multi-Dimensional Arrays: The "Array of Arrays" Illusion
+//        In languages like C/C++, a 2D array is a single, flat contiguous block of memory mapped mathematically. In Java, 2D arrays do not exist.
+//        Java implements 2D arrays as an "Array of Arrays".
+//                When you declare int[][] arr = new int[3][4];:
+//        Java creates a 1D array of size 3 in the Heap.
+//                However, this array does not store integers. It stores reference variables (pointers, which take 4 bytes each).
+//        Each of those 3 reference variables points to another completely separate 1D integer array of size 4 somewhere else in the Heap.
+//                Because of this, the sub-arrays in a Java 2D array do not have to be contiguous with each other in memory, and they don't even have to be the same size (Jagged Arrays).
+    }
+}
