9256c4f66d4273387e977e3d6fdf00ccf589c893
Sahil Deshmukh <sahildeshmukhofficial45@gmail.com>
2026-08-11 23:28:48 +0530

    Object class

--- 7.OOP/src/ObjectClass.java
+++ 7.OOP/src/ObjectClass.java
@@ -1,2 +1,5 @@
 public class ObjectClass {
+//    In Java, the Object class (located in the java.lang package) is the cosmic parent.
+//     by default in java every class extends Object this is the main parent class
+//    Object provides
 }
